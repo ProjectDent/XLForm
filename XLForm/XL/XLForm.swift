db05ee864a92b9687f6276559bdc2706e8c7a7f2
Andrew Hart <Andrew@ProjectDent.com>
2014-10-13 00:25:06 +0100

    adds swift support

---
+++ XLForm/XL/XLForm.swift
@@ -0,0 +1,42 @@
+//
+//  XLFormAdditions.swift
+//  TodayTwo
+//
+//  Created by Andrew Hart on 09/10/2014.
+//  Copyright (c) 2014 Project Dent. All rights reserved.
+//
+
+import Foundation
+
+var XLFormRowDescriptorTypeText = "text"
+var XLFormRowDescriptorTypeName = "name"
+var XLFormRowDescriptorTypeURL = "url"
+var XLFormRowDescriptorTypeEmail = "email"
+var XLFormRowDescriptorTypePassword = "password"
+var XLFormRowDescriptorTypeNumber = "number"
+var XLFormRowDescriptorTypePhone = "phone"
+var XLFormRowDescriptorTypeTwitter = "twitter"
+var XLFormRowDescriptorTypeAccount = "account"
+var XLFormRowDescriptorTypeInteger = "integer"
+var XLFormRowDescriptorTypeTextView = "textView"
+var XLFormRowDescriptorTypeSelectorPush = "selectorPush"
+var XLFormRowDescriptorTypeSelectorActionSheet = "selectorActionSheet"
+var XLFormRowDescriptorTypeSelectorAlertView = "selectorAlertView"
+var XLFormRowDescriptorTypeSelectorPickerView = "selectorPickerView"
+var XLFormRowDescriptorTypeSelectorPickerViewInline = "selectorPickerViewInline"
+var XLFormRowDescriptorTypeMultipleSelector = "multipleSelector"
+var XLFormRowDescriptorTypeSelectorLeftRight = "selectorLeftRight"
+var XLFormRowDescriptorTypeSelectorSegmentedControl = "selectorSegmentedControl"
+var XLFormRowDescriptorTypeDateInline = "dateInline"
+var XLFormRowDescriptorTypeDateTimeInline = "datetimeInline"
+var XLFormRowDescriptorTypeTimeInline = "timeInline"
+var XLFormRowDescriptorTypeDate = "date"
+var XLFormRowDescriptorTypeDateTime = "datetime"
+var XLFormRowDescriptorTypeTime = "time"
+var XLFormRowDescriptorTypeDatePicker = "datePicker"
+var XLFormRowDescriptorTypePicker = "picker"
+var XLFormRowDescriptorTypeBooleanCheck = "booleanCheck"
+var XLFormRowDescriptorTypeBooleanSwitch = "booleanSwitch"
+var XLFormRowDescriptorTypeButton = "button"
+var XLFormRowDescriptorTypeImage = "image"
+var XLFormRowDescriptorTypeStepCounter = "stepCounter"
